e4ba83905d13395619ee96952bbc3fe82a6ddca9
AnatolSalau <salovanatoly5@mail.ru>
2022-04-13 21:50:31 +0300

    Classwork

---
+++ src/CW_3_1_MultidimensionArray/Main.java
@@ -0,0 +1,33 @@
+package CW_3_1_MultidimensionArray;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        consolePrintMultiDimensionArray(getMultiDimensionArray(4,4));
+
+    }
+
+    public static int[][] getMultiDimensionArray(int width, int height) {
+        int[][] array = new int[width][height];
+        return array;
+    }
+
+    public static void consolePrintMultiDimensionArray(int[][] array) {
+        System.out.println(Arrays.deepToString(array));
+    }
+
+    public static void findAndPrintIndexStringWithMaxMultiple (int[][] array) {
+        int maxMultiple = 0;
+        //int indexOfMaxMultipleString
+        for (int[] arr : array) {
+            int resultMultiple = 1;
+            for (int val:arr) {
+                resultMultiple *= val;
+            }
+        if (maxMultiple > resultMultiple) {
+            maxMultiple = resultMultiple ;
+        }};
+            System.out.println();
+    }
+}
--- src/CW_3_MultidimensionalArray/Main.java
+++ src/CW_3_MultidimensionalArray/Main.java
@@ -3,9 +3,18 @@ package CW_3_MultidimensionalArray;
 public class Main {
     public static void main(String[] args) {
 
+        getArray();
+        printArray(getArray());
     }
-    public static int[] getArray() {
 
+    public static int[][][][][][][][] getArray() {
+        int[][][][][][][][] myArray = new int[8][8][8][8][8][8][8][8];
+        return myArray;
+    }
+    public static void printArray(int[][][][][][][][] myArray) {
+        for (int[][][][][][][] arr: myArray) {
+            System.out.println(arr.toString());
+        }
     }
 }
 
