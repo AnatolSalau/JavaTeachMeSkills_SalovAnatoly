ba210ce042e74b27f537deb2878de82e15c5a130
AnatolSalau <salovanatoly5@mail.ru>
2022-04-26 11:19:30 +0300

    Add HomeWork 8 OOP MainTask

---
+++ src/HW_8_OOP/MainTask_1_Phone/Main.java
@@ -0,0 +1,51 @@
+package HW_8_OOP.MainTask_1_Phone;
+
+public class Main {
+    public static void main(String[] args) {
+/*        1. Класс Phone.
+        Создайте класс Phone, который содержит переменные number, model и
+        weight.
+                Создайте три экземпляра этого класса.
+        Выведите на консоль значения их переменных.
+        Добавить в класс Phone методы: receiveCall, имеет один параметр – имя
+        звонящего.Выводит на консоль сообщение “Звонит {name}”. getNumber –
+        возвращает номер телефона. Вызвать эти методы для каждого из
+        объектов.
+                Добавить конструктор в класс Phone, который принимает на вход три
+        параметра для инициализации переменных класса - number, model и
+        weight.
+                Добавить конструктор, который принимает на вход два параметра для
+        инициализации переменных класса - number, model.
+                Добавить конструктор без параметров.
+                Вызвать из конструктора с тремя параметрами конструктор с двумя.
+        Добавьте перегруженный метод receiveCall, который принимает два
+        параметра - имя звонящего и номер телефона звонящего. Вызвать этот
+        метод.
+                Создать метод sendMessage с аргументами переменной длины. Данный
+        метод принимает на вход номера телефонов, которым будет отправлено
+        сообщение. Метод выводит на консоль номера этих телефонов.*/
+
+        Phone phoneOne = new Phone(11111,"Nokia", 8.97);
+        Phone phoneTwo = new Phone(22222,"Iphone", 9.15);
+        Phone phoneThree = new Phone(22222,"Iphone", 12);
+
+        Phone.printConsole(phoneOne,phoneTwo,phoneThree);
+
+        phoneOne.receiveCall("Anatoly");
+        phoneTwo.receiveCall("Vasia");
+        phoneThree.receiveCall("Vika");
+
+        System.out.println();
+        System.out.println(phoneOne.getNumber());
+        System.out.println(phoneTwo.getNumber());
+        System.out.println(phoneThree.getNumber());
+
+        phoneOne.receiveCall("Anatoly",235);
+        phoneTwo.receiveCall("Vasia", 547);
+        phoneThree.receiveCall("Vika", 568);
+
+        phoneOne.sendMessage(12345,45678,789568);
+
+
+    }
+}
